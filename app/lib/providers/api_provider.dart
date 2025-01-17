c0e9404f9e04bcbc9335b12ce2600b8aec31f425
Karol <karolmalicki.001@gmail.com>
2025-01-17 08:14:57 +0100

    created add document

--- app/lib/providers/api_provider.dart
+++ app/lib/providers/api_provider.dart
@@ -39,6 +39,59 @@ class ApiProvider with ChangeNotifier {
     }
   }
 
+  Future<void> uploadDocument(Uint8List fileBytes, String fileName) async {
+    var url = Uri.parse('https://government-assistant-api-183025368636.us-central1.run.app/validate-document');
+    try {
+      _addMessage(Message(
+        message: 'Uploading document...',
+        isUserMessage: true,
+      ));
+
+      String mimeType = _detectMimeType(fileName);
+
+      var request = http.MultipartRequest('POST', url);
+      request.files.add(http.MultipartFile.fromBytes(
+        'file',
+        fileBytes,
+        filename: fileName,
+        contentType: MediaType.parse(mimeType),
+      ));
+
+      var streamedResponse = await request.send();
+      var response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        var responseBody = utf8.decode(response.bodyBytes);
+        var responseData = json.decode(responseBody);
+
+        String resultMessage = responseData['content'] ?? 'No content available';
+        _addMessage(Message(
+          message: resultMessage,
+          isUserMessage: false,
+          isMarkdown: true,
+        ));
+      } else if (response.statusCode == 400) {
+        var responseBody = utf8.decode(response.bodyBytes);
+        var responseData = json.decode(responseBody);
+
+        _addMessage(Message(
+          message: responseData['detail'] ?? 'Unsupported file type.',
+          isUserMessage: false,
+        ));
+      } else {
+        _addMessage(Message(
+          message: 'Document upload failed: ${response.reasonPhrase}',
+          isUserMessage: false,
+        ));
+      }
+    } catch (e) {
+      _addMessage(Message(
+        message: 'Error occurred: $e',
+        isUserMessage: false,
+      ));
+    }
+  }
+
   Future<void> generateResponse(String question) async {
   var url = Uri.parse('https://government-assistant-api-183025368636.us-central1.run.app/generate-response');
   
@@ -92,5 +145,15 @@ class ApiProvider with ChangeNotifier {
   void _setLoading(bool isLoading) {
     _isLoading = isLoading;
     notifyListeners();
+  }
+
+  String _detectMimeType(String fileName) {
+    if (fileName.endsWith('.pdf')) {
+      return 'application/pdf';
+    } else if (fileName.endsWith('.docx')) {
+      return 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+    } else {
+      throw Exception('Unsupported file type. Only PDF and DOCX are allowed.');
+    }
   }
 }
